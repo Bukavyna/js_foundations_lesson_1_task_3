c7bcbea18e6c87bd1c8e9f796e6a58647373634b
Taras <luchakt2@gmail.com>
2023-08-27 16:20:56 +0300

    end

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+
+let numberPens = 4;
+let costPen = 5.25;
+let costPenS = numberPens * costPen;
+console.log(costPenS);
+
+let numberPencil = 6;
+let costPencil = 3.5;
+let costPencilS = numberPencil * costPencil;
+console.log(costPencilS);
+
+let result = costPenS + costPencilS;
+console.log(result);
